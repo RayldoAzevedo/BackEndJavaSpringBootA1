d064aeb9b479036f8854cf6cf861592a56ec0782
Rayldo Azevedo <azevedorayldo@gmail.com>
2022-04-15 12:09:16 -0300

    tutorialslide

---
+++ src/main/java/br/com/azevedo/rayldo/usuariobdh2/Usuariobdh2Application.java
@@ -0,0 +1,13 @@
+package br.com.azevedo.rayldo.usuariobdh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Usuariobdh2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Usuariobdh2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/azevedo/rayldo/usuariobdh2/Usuariobdh2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.azevedo.rayldo.usuariobdh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Usuariobdh2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
